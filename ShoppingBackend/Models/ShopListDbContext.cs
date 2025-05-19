f974011efe600b0ff20a3b8d077e03e826bec867
tonyk <tonyk@TONY>
2025-05-19 17:47:24 +0300

    Added azure database connection string

--- ShoppingBackend/Models/ShopListDbContext.cs
+++ ShoppingBackend/Models/ShopListDbContext.cs
@@ -18,9 +18,16 @@ public partial class ShopListDbContext : DbContext
     public virtual DbSet<Shoplist> Shoplist { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
- //To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=Tony\\SQLEXPRESS; Database=ShopListDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        //To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+            
+            //Paikallinen tietokanta
+        //=> optionsBuilder.UseSqlServer("Data Source=Tony\\SQLEXPRESS; Database=ShopListDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+            
+            //Azure tietokanta
+        => optionsBuilder.UseSqlServer("Server=tcp:shoppingapp.database.windows.net,1433;Initial Catalog=ShopListDB;Persist Security Info=False;User ID=TonyK;Password=Spiderman989;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
 
+    
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Shoplist>(entity =>
